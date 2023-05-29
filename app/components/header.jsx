5b7121227690a197057dbc61c25af0155191748a
luchofazio27 <luchofazio@gmail.com>
2023-05-28 23:34:44 -0300

    agregando estilos al header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,13 +1,14 @@
-import { Link } from "@remix-run/react" 
+import { Link } from "@remix-run/react"
+import logo from '../../public/img/logo.svg' 
 
 function Header() {
   return (
     <header className="header">
-        <div className="contenedor barra">
-            <div className="logo">
-
-            </div>
-            <nav className="navegacion">
+        <div className="container navbar">
+            <Link to='/'>
+                <img className="logo" src={logo} alt='logo image' />
+            </Link>
+            <nav className="navigation">
                 <Link to="/">Inicio</Link>
                 <Link to="/we">Nosotros</Link>
                 <Link to="/store">Tienda</Link>
